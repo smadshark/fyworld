f6df88346894ad66a46d386470b149d44fdc3df5
smad shark <smadshark@gmail.com>
2023-03-19 02:00:42 +0900

    append singleton test code

--- server/pure/src/test/java/practice/pure/singleton/SingletonTest.java
+++ server/pure/src/test/java/practice/pure/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package practice.pure.singleton;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
@@ -12,5 +14,9 @@ public class SingletonTest {
 
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same: ==
+        // equal: equals
     }
 }
