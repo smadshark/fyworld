cab2c57896846b14873264f09137b655beb58e4e
sanggyun <sanggyun@useb.co.kr>
2023-04-26 16:17:11 +0900

    Product DAOImpl

--- server/jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
+++ server/jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -6,6 +6,9 @@ import com.springboot.jpa.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Component
 public class ProductDAOImpl implements ProductDAO {
     private final ProductRepository productRepository;
@@ -17,21 +20,47 @@ public class ProductDAOImpl implements ProductDAO {
 
     @Override
     public Product insertProduct(Product product) {
-        return null;
+        Product saveProduct = productRepository.save(product);
+
+        return saveProduct;
     }
 
     @Override
     public Product selectProduct(Long number) {
-        return null;
+        Product selectedProduct = productRepository.getReferenceById(number);
+
+        return selectedProduct;
     }
 
     @Override
     public Product updateProductName(Long number, String name) throws Exception {
-        return null;
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdatedAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+
+        return updatedProduct;
     }
 
     @Override
     public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
 
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
     }
 }
