6767fdcaaef9ad647e657d692535ca59dd9f9116
smad shark <smadshark@gmail.com>
2023-04-28 01:38:28 +0900

    not working test... skip...

---
+++ server/jpa/src/test/java/com/springboot/jpa/controller/ProductControllerTest.java
@@ -0,0 +1,44 @@
+package com.springboot.jpa.controller;
+
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.service.impl.ProductServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ProductServiceImpl productService;
+
+    @Test
+    void getProduct() throws Exception {
+        given(productService.getProduct(1L))
+                .willReturn(new ProductResponseDto(1L, "pen", 5000, 2000));
+
+        String productId = "1";
+
+        mockMvc.perform(get("/product?number=?" + productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+
+        verify(productService).getProduct(1L);
+    }
+}
